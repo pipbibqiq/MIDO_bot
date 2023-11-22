54694687375195fa1032b50442af8f6b9858a6ef
pipbibqiq <pipbibqiq@mail.ru>
2023-11-22 20:56:38 +0300

    ...

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import TOKEN_API
-from poit.poit_course_selection import ikb_poit_course_selection
 
 import text_messages
 
@@ -18,25 +17,111 @@ async def on_startup(_):
     print('The bot has been successfully launched!')
 
 
-#Выбор специальности МИДО
-ikb_mido = InlineKeyboardMarkup(row_width=2)
-ib_mido_poit = [InlineKeyboardButton(text='ПОИТ', callback_data='mido_poit')]
-ikb_mido.add(*ib_mido_poit)
+# Кнопка "Начать!"
+ikb_mido_start = InlineKeyboardMarkup(row_width=2)
+ib_mido_poit = [InlineKeyboardButton(text='Начать!', callback_data='mido_poit_start')]
+ikb_mido_start.add(*ib_mido_poit)
 
 
-#Выбор ПОИТ
-@dp.callback_query_handler(lambda callback_query: callback_query.data == 'mido_poit')
+# Обработка кнопки "Начать!"
+@dp.callback_query_handler(lambda callback_query: callback_query.data == 'mido_poit_start')
 async def handle_button(callback_query: types.CallbackQuery):
     await callback_query.answer()  # Ответить на запрос обратного вызова, чтобы убрать "часики" у кнопки
     await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)  # Удалить старое сообщение с кнопками
-    await bot.send_message(callback_query.from_user.id, 'Вы выбрали ПОИТ!\nВыберите какой у вас курс:', reply_markup=ikb_poit_course_selection)
+    await bot.send_message(callback_query.from_user.id, 'Успешно!\nВыберите какой вопрос вас интересует:', reply_markup=ikb_selection)
+
+
+# Выбор информации
+ikb_selection = InlineKeyboardMarkup(row_width=2)
+ib_generalScheduleSheet = [InlineKeyboardButton(text='Расписание', callback_data='poit_generalScheduleSheet')]
+ib_interSessionSchedule = [InlineKeyboardButton(text='Расписание межсессионных занятий', callback_data='poit_interSessionSchedule')]
+ib_sessionSchedule = [InlineKeyboardButton(text='График проведения сессий', callback_data='poit_sessionSchedule')]
+ib_listOfTeachers = [InlineKeyboardButton(text='Контакты преподавателей', callback_data='poit_listOfTeachers')]
+ib_liquidationOfDebts = [InlineKeyboardButton(text='Ликвидация долгов', callback_data='poit_liquidationOfDebts')]
+ib_paymentSchedule = [InlineKeyboardButton(text='График оплат', callback_data='poit_paymentSchedule')]
+ikb_selection.add(*ib_generalScheduleSheet)
+ikb_selection.add(*ib_interSessionSchedule)
+ikb_selection.add(*ib_sessionSchedule)
+ikb_selection.add(*ib_listOfTeachers)
+ikb_selection.add(*ib_liquidationOfDebts)
+ikb_selection.add(*ib_paymentSchedule)
+
+
+# 1.1 Функция отправки фотографии .jpg с информацие о расписании занятий
+@dp.callback_query_handler(lambda callback_query: callback_query.data == 'poit_generalScheduleSheet')
+async def handle_button_gss(callback_query: types.CallbackQuery):
+    await callback_query.answer()  # Ответить на запрос обратного вызова, чтобы убрать "часики" у кнопки
+    await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)  # Удалить старое сообщение с кнопками
+    # Отправка фото из папки poit_1course
+    photo_path = "data/schedule.jpg"
+    with open(photo_path, "rb") as photo_file:
+        await bot.send_photo(callback_query.from_user.id, photo_file, caption="Общий лист расписания.")
+    await bot.send_message(callback_query.from_user.id, 'Выберите следующее действие:', reply_markup=ikb_back)
+
+
+# 1.2 Функция обработки межсессионного расписания
+@dp.callback_query_handler(lambda callback_query: callback_query.data == 'poit_interSessionSchedule')
+async def handle_button_iss(callback_query: types.CallbackQuery):
+    await callback_query.answer()  # Ответить на запрос обратного вызова, чтобы убрать "часики" у кнопки
+    await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)  # Удалить старое сообщение с кнопками
+    await bot.send_message(callback_query.from_user.id, text='Расписание межсессионных занятий.\n' + text_messages.NO_DATA)
+    await bot.send_message(callback_query.from_user.id, 'Выберите следующее действие:', reply_markup=ikb_back)
+
+
+# 1.3 Функция обработки график сессий
+@dp.callback_query_handler(lambda callback_query: callback_query.data == 'poit_sessionSchedule')
+async def handle_button_ss(callback_query: types.CallbackQuery):
+    await callback_query.answer()  # Ответить на запрос обратного вызова, чтобы убрать "часики" у кнопки
+    await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)  # Удалить старое сообщение с кнопками
+    await bot.send_message(callback_query.from_user.id, text='График проведения сессий.\n' + text_messages.NO_DATA)
+    await bot.send_message(callback_query.from_user.id, 'Выберите следующее действие:', reply_markup=ikb_back)
+
+
+# 1.4 Функция обработки контактов преподавателей
+@dp.callback_query_handler(lambda callback_query: callback_query.data == 'poit_listOfTeachers')
+async def handle_button_lot(callback_query: types.CallbackQuery):
+    await callback_query.answer()  # Ответить на запрос обратного вызова, чтобы убрать "часики" у кнопки
+    await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)  # Удалить старое сообщение с кнопками
+    await bot.send_message(callback_query.from_user.id, text='Контакты преподавателей.\n' + text_messages.NO_DATA)
+    await bot.send_message(callback_query.from_user.id, 'Выберите следующее действие:', reply_markup=ikb_back)
+
+
+# 1.5 Функция обработки ликвидации долгов
+@dp.callback_query_handler(lambda callback_query: callback_query.data == 'poit_liquidationOfDebts')
+async def handle_button_lod(callback_query: types.CallbackQuery):
+    await callback_query.answer()  # Ответить на запрос обратного вызова, чтобы убрать "часики" у кнопки
+    await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)  # Удалить старое сообщение с кнопками
+    await bot.send_message(callback_query.from_user.id, text='Ликвидация долгов.\n' + text_messages.NO_DATA)
+    await bot.send_message(callback_query.from_user.id, 'Выберите следующее действие:', reply_markup=ikb_back)
+
+
+# 1.6 Функция обработки графика оплат
+@dp.callback_query_handler(lambda callback_query: callback_query.data == 'poit_paymentSchedule')
+async def handle_button_1course_ps(callback_query: types.CallbackQuery):
+    await callback_query.answer()  # Ответить на запрос обратного вызова, чтобы убрать "часики" у кнопки
+    await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)  # Удалить старое сообщение с кнопками
+    await bot.send_message(callback_query.from_user.id, text='График оплат.\n' + text_messages.NO_DATA)
+    await bot.send_message(callback_query.from_user.id, 'Выберите следующее действие:', reply_markup=ikb_back)
+
+# Вернуться к выбору интересующей информации
+ikb_back = InlineKeyboardMarkup(row_width=2)
+ib_mido_back = [InlineKeyboardButton(text='Вернуться в начало', callback_data='back')]
+ikb_back.add(*ib_mido_back)
+
+
+# Возврат в начало
+@dp.callback_query_handler(lambda callback_query: callback_query.data == 'back')
+async def handle_button_back(callback_query: types.CallbackQuery):
+    await callback_query.answer()
+    await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
+    await bot.send_message(callback_query.from_user.id, text=text_messages.START_COMMAND, reply_markup=ikb_mido_start)  # Отправляем команду /start
 
 
 # Обработчик всех текстовых сообщений (удаляет любые сообщения, кроме команды /start)
 @dp.message_handler(content_types=types.ContentType.TEXT)
 async def handle_text_messages(message: types.Message):
     if message.text.strip().lower() == "/start":
-        await message.answer(text=text_messages.START_COMMAND, reply_markup=ikb_mido)
+        await message.answer(text=text_messages.START_COMMAND, reply_markup=ikb_mido_start)
         await message.delete()
     else:
         await message.delete()
@@ -63,16 +148,16 @@ async def handle_media_messages(message: types.Message):
     await message.delete()
 
 
-#Команда /help
+# Команда /help
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
     await message.answer(text=text_messages.HELP_COMMAND)
 
 
-#Команда /start
+# Команда /start
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.answer(text=text_messages.START_COMMAND, reply_markup=ikb_mido)
+    await message.answer(text=text_messages.START_COMMAND, reply_markup=ikb_mido_start)
     await bot.delete_message(message.chat.id, message.message_id)
 
 
--- poit/__init_.py
+++
@@ -1,5 +0,0 @@
-from . import poit_1st_course
-from poit_2nd_course import ikb_poit_2nd_course
-from poit_3rd_course import ikb_poit_3rd_course
-from poit_4th_course import ikb_poit_4th_course
-from poit_5th_course import ikb_poit_5th_course
